67b2723893ea9c137aa8d56679b823341cf7ec1d
hotyo <moeuvozhenie@tut.by>
2020-10-11 01:28:59 +0300

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let pair = bracketsConfig[i].map(element => !isNaN(+element) ? element : `\\${element}`);
+    newStr = str.replace(new RegExp(pair.join(''), 'g'), '');
+    if (newStr !== str)
+      break;
+  }
+  if (!newStr.length)
+    return true;
+  else {
+    if (newStr === str)
+      return false;
+    else
+      return check(newStr, bracketsConfig)
+  }
 }
